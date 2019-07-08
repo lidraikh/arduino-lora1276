ff4928254798f87a9130d5df7264def1e79a764d
Leo Idraikh <leo@cctronic.com>
2019-07-08 01:17:02 -0400

    Create mgos_arduino_lora1276.h

---
+++ Include/mgos_arduino_lora1276.h
@@ -0,0 +1,19 @@
+/*
+ * Arduino lora1276 library API wrapper
+ */
+
+#ifndef _MGOS_ARDUINO_LORA1276_H_
+#define _MGOS_ARDUINO_LORA1276_H_
+
+
+
+#ifdef __cplusplus
+#include "LORA.h"
+#else
+typedef struct LORATag LORA;
+#endif
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
