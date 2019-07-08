9fab7259a158423106fc4b46fbcc0b07e5e1ff86
Leo Idraikh <leo@cctronic.com>
2019-07-08 00:56:12 -0400

    Add files via upload

---
+++ examples/lora_rx/lora_rx.ino
@@ -0,0 +1,41 @@
+// lora_rx.pde
+
+// Caution:lora can only work under 3.3V
+// please make sure the supply of you board is under 3.3V
+// 5v supply will destroy lora module!!
+
+// This code runs in rx mode and  works with lora_tx.pde 
+// Flow:receive packet from tx->print to serial
+// data of packet is "swwxABCDEFGHIm"
+
+#include<LORA.h>
+#include <SPI.h>
+#include <SoftwareSerial.h>
+LORA lora;
+
+unsigned char flag=1;    //  flag of rx mode
+unsigned char rx_len;
+unsigned char rx_buf[20];
+
+void setup() {
+  Serial.begin(9600);
+  if(!lora.init())
+  {
+     Serial.println("Init fail!");
+  }
+   lora.rxInit();    // wait for packet from master
+}
+void loop() 
+{
+    if(flag==1)
+    {
+        if(lora.waitIrq(LR_RXDONE_MASK))    // wait for RXDONE interrupt
+        {
+            lora.clrInterrupt();
+            rx_len=lora.rxPacket(rx_buf);  // read rx data
+            Serial.write(rx_buf,rx_len);    // print out by serial
+            Serial.println();
+            lora.rxInit();    // wait for packet from master
+        }
+    }
+}
---
+++ examples/lora_tx/lora_tx.ino
@@ -0,0 +1,34 @@
+// lora_tx.pde
+
+// Caution:lora module can only work under 3.3V
+// please make sure the supply of you board is under 3.3V
+// 5v supply will destroy lora module!!
+
+// This code runs in tx mode and  works with lora_rx.pde 
+// Flow:receive "T" from serial->send a packet
+// data of packet is "swwxABCDEFGHIm"
+
+#include <LORA.h>
+#include <SPI.h>
+#include <SoftwareSerial.h>
+LORA lora;
+unsigned char tx_buf[]={"swwxABCDEFGHIm"};
+unsigned char val;
+
+void setup() {
+  Serial.begin(9600);
+  if(!lora.init())
+  {
+     Serial.println("Init fail!");
+  }
+}
+void loop() 
+{
+    val=Serial.read();  // please make sure serial is OK befor runing this code
+    if(val=='T')    // tx a packet if receive "T"
+    {
+       lora.txPacket(tx_buf,sizeof(tx_buf));
+       lora.enterStbyMode();    // turn to standby mode 
+       Serial.println("tx");
+    }
+}
